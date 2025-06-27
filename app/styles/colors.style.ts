c94960e5e4f6a7d162a47f2cc7cd8b177a3cf2e0
GianlucaGiordano <Gianluca.Giordano@emea.nttdata.com>
2025-06-27 16:59:19 +0200

    create hotelScreen and add asset

---
+++ app/styles/colors.style.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  APP_BACKGROUND: '#f7f7f7',
+  WHITE: '#fff',
+  BLACK: '#000',
+  GREEN: '#27a135',
+  GREY: '#757575',
+  DIVIDER: '#ececec',
+  RED: '#e53935',
+  BUTTON: '#1976d2',
+};
